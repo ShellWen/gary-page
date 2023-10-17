92a6ffbd44fcc6d2e3fca41cb25d2e1c8227e1f1
ShellWen <me@shellwen.com>
2023-10-17 21:15:52 +0800

    :rotating_light: Remove ESLint TypeScript check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
+    // 'plugin:@typescript-eslint/recommended-type-checked',
+    // 'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
